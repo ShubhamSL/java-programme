69cd55316da7ca9f079cbd6a52b1713941b926fe
Shubham Shrinivas Lahande <45897511+ShubhamSL@users.noreply.github.com>
2020-10-01 17:03:49 +0530

    Add files via upload

---
+++ ExceptionHandling.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class ExceptionHandling
+{
+    public static void main(String[] args)
+    {
+          Scanner sc = new Scanner(System.in);  //Declaring Scanner variable to take input from user
+ 
+          System.out.println("Enter Your Age");
+ 
+          int age = sc.nextInt();         //Taking input from user
+ 
+          try
+          {
+              if(age < 0)
+              {
+                  throw new Exception();
+                                      //throws an Exception if age is negative
+              }
+              else
+              {
+                  age = age * age;
+                  System.out.println("the square of nu is :"+ age);
+              }
+
+
+          }
+          catch(Exception ex)
+          {
+              System.out.println(ex);     //Prints exception description
+          }
+    }
+ }
